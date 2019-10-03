3e6d5aba47e493d474a448e5e13317e8b81c0d3d
Renan Zanelatto <renansz@gmail.com>
2019-10-03 13:01:06 -0300

    Added battery-widget plugin
    fixed autorun.sh to configure touchpad

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -7,5 +7,15 @@ function run {
 }
 
 if xrandr | grep -q 'DP-0 connected' ; then
-    xrandr --output DP-0 --mode 1280x1024 --rate 60.00 --output eDP-1-1 --right-of DP-0 --mode 1920x1080 --rate 60.00 --primary
+    xrandr --output eDP-0 --mode 1920x1080 --rate 60.00 --output HDMI-1 --right-of eDP-0 --mode 1920x1080 --rate 60.00 --primary
 fi
+
+# Touchpad device.  Use 'xinput' to find the name.
+# To view property values:  xinput list-props "$TOUCHPAD"
+TOUCHPAD="SynPS/2 Synaptics TouchPad"
+
+# Turn on palm detection for anything larger that "5".
+xinput set-prop "$TOUCHPAD" "Synaptics Palm Detection" 1
+xinput set-prop "$TOUCHPAD" "Synaptics Palm Dimensions" 10 1
+xinput set-prop "$TOUCHPAD" "Synaptics Edge Scrolling" 0, 0, 0
+#xinput set-prop "$TOUCHPAD" "Synaptics Area" 130 3130 0 0
