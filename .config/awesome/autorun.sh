109559c295465e2850bd47c56dd8cfa7db5d2b5a
Renan Zanelatto <renansz@gmail.com>
2019-10-11 22:12:22 -0300

    Added lenovo trackpoint stuff

--- .config/awesome/autorun.sh
+++ .config/awesome/autorun.sh
@@ -6,8 +6,8 @@ function run {
     fi
 }
 
-if xrandr | grep -q 'DP-0 connected' ; then
-    xrandr --output eDP-0 --mode 1920x1080 --rate 60.00 --output HDMI-1 --right-of eDP-0 --mode 1920x1080 --rate 60.00 --primary
+if xrandr | grep -q 'HDMI-2 connected' ; then
+    xrandr --output eDP-1 --mode 1920x1080 --rate 60.00 --output HDMI-2 --right-of eDP-1 --mode 1920x1080 --rate 60.00 --primary
 fi
 
 # Touchpad device.  Use 'xinput' to find the name.
@@ -16,6 +16,7 @@ TOUCHPAD="SynPS/2 Synaptics TouchPad"
 
 # Turn on palm detection for anything larger that "5".
 xinput set-prop "$TOUCHPAD" "Synaptics Palm Detection" 1
-xinput set-prop "$TOUCHPAD" "Synaptics Palm Dimensions" 10 1
+xinput set-prop "$TOUCHPAD" "Synaptics Palm Dimensions" 5 1
 xinput set-prop "$TOUCHPAD" "Synaptics Edge Scrolling" 0, 0, 0
+xinput set-prop "$TOUCHPAD" "Synaptics Tap Action" 0, 0, 0, 0, 0, 0, 0
 #xinput set-prop "$TOUCHPAD" "Synaptics Area" 130 3130 0 0
